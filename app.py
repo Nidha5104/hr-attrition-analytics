fa62402e975cfc3e0b10e681bb4dace011766d78
Fathima Nidha <140501253+Nidha5104@users.noreply.github.com>
2026-04-06 20:15:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="HR Analytics System",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("💼 HR Attrition Intelligence System")
+
+st.markdown("""
+### Welcome 👋
+
+This system helps HR teams:
+
+- 📊 Analyze employee attrition
+- 🤖 Predict employee risk
+- 💰 Estimate business impact
+
+Use sidebar to navigate:
+➡ Dashboard  
+➡ Prediction
+""")
