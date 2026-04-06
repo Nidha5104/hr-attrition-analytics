e2655b240a8901cc18f29307c81b45dc8e4f6981
Fathima Nidha <140501253+Nidha5104@users.noreply.github.com>
2026-04-06 20:20:25 +0530

    Create 1_Dashboard.py

---
+++ pages/1_Dashboard.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title("📊 HR Attrition Dashboard")
+
+df = pd.read_csv("dataset.csv")
+
+# KPI
+attrition_rate = df['Attrition'].value_counts(normalize=True)[1] * 100
+avg_income = df['MonthlyIncome'].mean()
+
+col1, col2 = st.columns(2)
+
+col1.metric("Attrition Rate (%)", f"{attrition_rate:.2f}")
+col2.metric("Average Income", f"{int(avg_income)}")
+
+st.divider()
+
+# Charts
+fig1 = px.histogram(df, x="Age", color="Attrition", title="Attrition by Age")
+st.plotly_chart(fig1, use_container_width=True)
+
+fig2 = px.histogram(df, x="Department", color="Attrition", title="Attrition by Department")
+st.plotly_chart(fig2, use_container_width=True)
+
+fig3 = px.box(df, x="Attrition", y="MonthlyIncome", title="Income Distribution")
+st.plotly_chart(fig3, use_container_width=True)
